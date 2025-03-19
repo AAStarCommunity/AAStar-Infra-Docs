33ff454eed56aae125ca89412ae1057ef6d29f8d
jhfnetboy <jhfnetboy@gmail.com>
2025-03-19 23:01:41 +0700

    init aastar docs again with docusaurus

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AirAccount-v0.2-design
 We begin to upgrade 0.1 to 0.2 with new features and more powerful infra
+
+## Tech stack
+
---
+++ docs.aastar.io/src/components/HomepageFeatures/index.js
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        AirAccount was designed from the ground up to be easily registered and
+        used to get your Web3 account up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on UX',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        SuperPaymaster and Cards system lets you focus on your transactions, and we&apos;ll do the chores. Go
+        ahead and move your transactions into the <code>Blockchain</code>.
+      </>
+    ),
+  },
+  {
+    title: 'Open Source and Transparent',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        All the code is open source and transparent. You can find the code on 
+        <a href="https://github.com/aastarcommunity">GitHub</a>.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
